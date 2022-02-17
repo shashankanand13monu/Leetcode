8f3d194ed44cd0518d1d204d384ce4ff6c1a618d
shashankanand13monu <79691562+shashankanand13monu@users.noreply.github.com>
2022-02-18 00:38:03 +0530

    Time: 9 ms (50.37%), Space: 13.8 MB (26.91%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -6,12 +6,19 @@ public:
        int max_far=INT_MIN;
      int min_far=1;
         int num=1;
-        
-        for(int i=0;i<nums.size();i++)
+        int n=1;
+        for(int i=0,j=nums.size()-1;i<nums.size(),j>=0;i++,j--)
            {
                   
             if(num==0)
                 num=1;
+            
+                 if(n==0)
+                n=1;
+               
+            n= n*nums[j];
+               
+            max_far= max(max_far,n);
             
            num= num*nums[i];
       
@@ -21,20 +28,20 @@ public:
            
            }
         num=1;
-           for(int i=nums.size()-1;i>=0;i--)
-           {
+//            for(int i=nums.size()-1;i>=0;i--)
+//            {
             
            
             
-            if(num==0)
-                num=1;
+//             if(num==0)
+//                 num=1;
                
-            num= num*nums[i];
+//             num= num*nums[i];
                
-            max_far= max(max_far,num);
+//             max_far= max(max_far,num);
             
            
-           }
+//            }
         
         return max_far;
     }
