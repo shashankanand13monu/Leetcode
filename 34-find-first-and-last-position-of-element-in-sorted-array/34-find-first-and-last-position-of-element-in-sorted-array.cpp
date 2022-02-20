de7893aedbce8bb8a33130c361b0b838f8b6291d
shashankanand13monu <79691562+shashankanand13monu@users.noreply.github.com>
2022-02-20 18:01:47 +0530

    Time: 16 ms (25.21%), Space: 13.5 MB (98.63%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -3,8 +3,48 @@ public:
     vector<int> searchRange(vector<int>& nums, int target) 
     {
         vector<int>ans(2,-1);
-        int id1=-1,id2;
+        int id1=-1,id2=-1;
+       
+        int lo=0,hi=nums.size()-1;
         
+        while(lo<=hi)
+        {
+            int mid = (lo+hi)/2;
+            
+            if(nums[mid]==target)
+            {
+                id1=mid;
+                id2=mid;
+               while(id1>0 and nums[id1-1]==target)
+                   id1--;
+                
+                while(id2<nums.size()-1 and nums[id2+1]==target)
+                    id2++;
+                 ans[0]=id1;
+            ans[1]=id2;
+                return ans;
+               
+            }
+                
+            else if(nums[mid]>target)
+                hi=mid-1;
+            else
+                lo=mid+1;
+        }
+        
+         
+      
+              ans[0]=id1;
+            ans[1]=id2;
+         
+           
+        
+            
+        
+        return ans;
+        
+        /*
+        // 2 Pass Start & End
         for(int i=0;i<nums.size();i++)
         {   
             
@@ -35,6 +75,7 @@ public:
             
         
         return ans;
+        */
         
         
     }
