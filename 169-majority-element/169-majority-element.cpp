44f2d2ddf6b65fdccfda83d5079d0285d10ae1a0
shashankanand13monu <79691562+shashankanand13monu@users.noreply.github.com>
2022-02-21 13:10:44 +0530

    Time: 34 ms (30.92%), Space: 19.6 MB (52.27%) - LeetHub

---
+++ 169-majority-element/169-majority-element.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& arr) {
+        
+    int i, candidate = -1, votes = 0;
+    // Finding majority candidate
+        int n= arr.size();
+    for (i = 0; i < n; i++) {
+        if (votes == 0) {
+            candidate = arr[i];
+            votes = 1;
+        }
+        else {
+            if (arr[i] == candidate)
+                votes++;
+            else
+                votes--;
+        }
+    }
+    int count = 0;
+    // Checking if majority candidate occurs more than n/2
+    // times
+    for (i = 0; i < n; i++) {
+        if (arr[i] == candidate)
+            count++;
+    }
+ 
+    if (count > n / 2)
+        return candidate;
+    return -1;
+    }
+};
