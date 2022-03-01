5401138d26e2a943a77c20a0b98959d929030685
shashankanand13monu <79691562+shashankanand13monu@users.noreply.github.com>
2022-03-01 18:43:16 +0530

    Time: 8 ms (28.80%), Space: 7.7 MB (79.51%) - LeetHub

---
+++ 46-permutations/46-permutations.cpp
@@ -0,0 +1,38 @@
+void dfs(vector<vector<int>> &ans ,vector<int>&t,map<int,int>&m,vector<int> &nums)
+{
+    if(t.size()==nums.size())
+    {   
+        
+        ans.push_back(t);
+        return;
+    }
+    
+    for(int i=0;i<nums.size();i++)
+    {
+        if(!m[i])
+        {   cout<<"h";
+            m[i]=1;
+            t.push_back(nums[i]);
+            dfs(ans,t,m,nums);
+            m[i]=0;
+            t.pop_back();
+        }
+    }
+    
+}
+
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) 
+    {
+        vector<vector<int>> ans;
+        vector<int>t;
+        map<int,int>m;
+        
+        dfs(ans,t,m,nums);
+        sort(ans.begin(),ans.end());
+        // ans.erase(unique(ans.begin(),ans.end()),ans.end());
+        return ans;
+        
+    }
+};
