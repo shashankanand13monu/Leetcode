d5f4b4a43037858a0f791c3fad8774eca7fc91dd
shashankanand13monu <79691562+shashankanand13monu@users.noreply.github.com>
2022-03-14 14:45:26 +0530

    Time: 4 ms (35.07%), Space: 7.6 MB (76.13%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,25 +1,32 @@
-int helper(int curr,vector<int>& nums,vector<int>& t)
-{
-    if(curr>=nums.size())
-        return 0;
+// int helper(int curr,vector<int>& nums,vector<int>& t)
+// {
+//     if(curr>=nums.size())
+//         return 0;
     
-    //Memorization
-    if(t[curr]!=-1)
-        return t[curr];
+//     //Memorization
+//     if(t[curr]!=-1)
+//         return t[curr];
     
-    int x= nums[curr] + helper(curr+2,nums,t);
-    int y= helper(curr+1,nums,t);
+//     int x= nums[curr] + helper(curr+2,nums,t);
+//     int y= helper(curr+1,nums,t);
     
-    return t[curr]=max(x,y);
+//     return t[curr]=max(x,y);
     
-}
+// }
 
 class Solution {
 public:
     int rob(vector<int>& nums) {
         
-        int n= nums.size();
-        vector<int>t(n,-1);
-        return helper(0,nums,t);
+       int n = nums.size(), pre = 0, cur = 0;
+        for (int i = 0; i < n; i++) {
+            int temp = max(pre + nums[i], cur);
+            pre = cur;
+            cur = temp;
+        }
+        return cur;
+        // int n= nums.size();
+        // vector<int>t(n,-1);
+        // return helper(0,nums,t);
     }
 };
