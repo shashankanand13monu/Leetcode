f098c6d22a9ba8c42b2444e5166685e099b863cc
shashankanand13monu <79691562+shashankanand13monu@users.noreply.github.com>
2022-03-23 13:07:23 +0530

    Time: 0 ms (100.00%), Space: 6 MB (30.21%) - LeetHub

---
+++ 991-broken-calculator/991-broken-calculator.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int brokenCalc(int startValue, int target) {
+        int res = 0;
+        
+        while(target > startValue)
+        {
+			// if target is odd we will make it even
+            if(target % 2)
+                ++target;
+			// if target is even divide by 2
+            else
+                target /= 2;
+            
+            ++res;
+        }
+        return res  + startValue - target;
+    
+    }
+};
