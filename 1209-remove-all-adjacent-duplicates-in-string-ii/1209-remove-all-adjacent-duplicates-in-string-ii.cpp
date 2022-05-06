31a818b25ce5c407aa31bfda89cfe4a8e7af63b5
shashankanand13monu <79691562+shashankanand13monu@users.noreply.github.com>
2022-05-06 10:20:20 +0530

    Time: 52 ms (24.11%), Space: 133.3 MB (5.44%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -7,14 +7,15 @@ public:
         for(auto i: s)
         {
             if(arr.empty() or arr.back().first!=i)
-                arr.push_back(make_pair(i,0));
-           
+                arr.push_back(make_pair(i,1));
+           else{
                 ++arr.back().second;
                 if(arr.back().second==k)
                     arr.pop_back();
+           }
             
         }
-        
+        cout<<string(0,'x');
         string ans="";
         for(auto j: arr)
         {
