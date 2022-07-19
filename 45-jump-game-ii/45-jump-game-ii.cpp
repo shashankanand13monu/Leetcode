7d5c026ec4e6cae5b1718eeba2ab89e40a018298
shashankanand13monu <79691562+shashankanand13monu@users.noreply.github.com>
2022-07-19 20:05:11 +0530

    Time: 27 ms (64.12%), Space: 16.4 MB (96.60%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) 
+    {   
+        if(nums.size()==1)
+            return 0;
+        int r=0;
+        int mx=0;
+        
+        int ans=0;
+        
+        for(int i=0;i<nums.size()-1;i++)
+        {
+            // if(nums[i]+i>r)
+            //     temp=nums[i]+i;
+            
+            
+            
+            mx=max(mx,i+nums[i]);
+            
+            if(i==r)
+            {
+                ans++;
+                // max=temp;
+                r=mx;
+            }
+        }
+        
+        return ans;
+    }
+};
