d8f11fbf3412e8cddcfa58b558a73bea4ee7ebaf
shashankanand13monu <79691562+shashankanand13monu@users.noreply.github.com>
2022-08-09 16:33:03 +0530

    Time: 23 ms (60.11%), Space: 8.7 MB (74.33%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -1,42 +1,31 @@
- int check(int a,int b,string x,string y,unordered_map<char,int> hash,unordered_map<char,int> phash)
-            {
-                for(int i=a;i<=b;i++)
-                {
-                    if(hash[y[i]]!=phash[y[i]])
-                        return -1;
-                }
-                return a;
-            }
-
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-       int s_len = s.length();
-        int p_len = p.length();
+        vector<int>ans;
+        vector<int>pcount(26,0);
+        vector<int>scount(26,0);
         
-        if(s.size() < p.size()) return {};
+           int m= s.size(),n=p.size();
+        if(n>m)
+            return {};
         
-        vector<int> freq_p(26,0);
-        vector<int> window(26,0);
+        for(auto i:p)
+            pcount[i-'a']++;
+        for(auto i:s.substr(0,n))
+            scount[i-'a']++;
         
-        //first window
-        for(int i=0;i<p_len;i++){
-            freq_p[p[i]-'a']++;
-            window[s[i]-'a']++;
-        }
+        if(pcount==scount)
+            ans.push_back(0);
         
-        vector<int> ans;
-        if(freq_p == window) ans.push_back(0);
-        
-        for(int i=p_len;i<s_len;i++){
-            window[s[i-p_len] - 'a']--;
-            window[s[i] - 'a']++;
+        for(int i=n;i<m;i++)
+        {
+            scount[s[i-n]-'a']--;
+            scount[s[i]-'a']++;
             
-            if(freq_p == window) ans.push_back(i-p_len+1);
+            if(pcount==scount)
+                ans.push_back(i-n+1);
+            
         }
         return ans;
-       
-           
-        
     }
 };
