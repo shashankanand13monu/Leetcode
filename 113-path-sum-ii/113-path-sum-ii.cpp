9f89135a19eff71b9cf5672c3706e40928f2fabb
shashankanand13monu <79691562+shashankanand13monu@users.noreply.github.com>
2022-09-24 22:44:42 +0530

    Time: 49 ms (7.14%), Space: 39 MB (6.18%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.cpp
+++ 113-path-sum-ii/113-path-sum-ii.cpp
@@ -12,22 +12,21 @@
 
 void pre(TreeNode* root,int sum, vector<int> a, vector<vector<int>> &ans)
 {
+  
     if(!root)
         return;
-    
+      a.push_back(root->val);
     if(!root->left and !root->right and sum-root->val==0)
     {
-        a.push_back(root->val);
         ans.push_back(a);
-        return;
-    }
         
+    }
     
-    a.push_back(root->val);
-    pre(root->left,sum - root->val ,a,ans);
-    pre(root->right,sum - root->val ,a,ans);
+  
+    pre(root->left,sum-root->val,a,ans); 
+    pre(root->right,sum-root->val,a,ans); 
+
     
-    a.pop_back();
     
 }
 
@@ -35,7 +34,7 @@ class Solution {
 public:
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) 
     {
-          vector<int>a;
+         vector<int>a;
          vector<vector<int>>ans;
         
         pre(root,targetSum,a,ans); 
